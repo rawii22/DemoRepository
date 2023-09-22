81395372983b9debbd4ff1e321805da9c380695f
Collin Cunningham <collin.cunning@outlook.com>
2023-09-21 19:50:46 -0700

    added two print statements

--- test.cpp
+++ test.cpp
@@ -6,6 +6,8 @@ int main()
 {
     cout << "Hello world!" << endl;
     cout << "Something else!" << endl;
+    cout << "Collin was here har har har" << endl;
+    cout << "Github rules!!!!" << endl;
 
     return 0;
 }
